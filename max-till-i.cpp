94af7e8906d0ed9d8ce1ac32a8ea135bb7589d61
Atharva Nanavate <85158270+AtharvaNanavate@users.noreply.github.com>
2022-07-20 00:04:03 +0530

    Add files via upload

---
+++ max-till-i.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void maxTill(int i, int arr[]){
+int max = -9999999;
+
+    for(int k = 0; k <= i; k++){
+        if(arr[k] > max){
+            max = arr[k];
+        }
+    }
+
+    cout << "Maximum Number till array element - " << i+1 << " : "<< max << endl;
+}
+
+int main(){
+
+    int n;
+    cout << "Enter the number of elements in your array : ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of your array : ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    for(int i = 0; i < n; i++){
+        maxTill(i,arr);
+    }
+    return 0;
+}
