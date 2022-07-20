a93ce768d0b8e2b40f1b533591eb8b176a16bb6f
Atharva Nanavate <85158270+AtharvaNanavate@users.noreply.github.com>
2022-07-20 22:48:37 +0530

    Add files via upload

---
+++ ArrayChallenges/sumOfSubsets.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout << "Enter the number of elements in your array : ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of your array : ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    cout << "Sum of all subsets : " << endl;
+    for(int i = 0; i < n; i++){
+        int sum = 0;
+        for(int j = i; j < n; j++){
+            sum+=arr[j];
+            cout << sum << endl;
+        }    
+    }
+    return 0;
+}
