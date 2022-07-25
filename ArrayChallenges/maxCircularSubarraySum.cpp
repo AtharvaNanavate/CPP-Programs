6c9c9ecf7b113d0e1e93b9305675abed53e0a213
Atharva Nanavate <85158270+AtharvaNanavate@users.noreply.github.com>
2022-07-25 15:24:18 +0530

    Add files via upload

---
+++ ArrayChallenges/maxCircularSubarraySum.cpp
@@ -0,0 +1,64 @@
+/* Problem : Find the subarray in an array which has maximum sum. Print the sum as output.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout << "Enter the number of elements in your array : ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of your array : ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    //kadane's algorithm
+    int sum[n], maximum = -99999999;
+    sum[0] = arr[0];
+    
+    if(arr[0] < 0){
+        sum[0] = 0;
+    }
+
+    int flag = 0;
+    for(int i = 1; i < n; i++){
+        sum[i] = sum[i-1] + arr[i];
+        if(sum[i] < 0){
+            sum[i] = 0;
+        }
+        if(i == n-1 && flag == 0){
+            i = -1;
+            flag = 1;
+        }
+        if(flag = 1 && i >= 0){
+            
+        }
+        
+        maximum = max(sum[i],maximum);
+    }
+    
+        if(arr[n-1] > 0 && arr[0] > 0){
+            int s = 0;
+            for(int i = 0; i < n; i++){
+                if(arr[i] < 0){
+                    break;
+                }
+                s += arr[i];
+            }
+            for(int i = n-1; i >= 0; i--){
+                if(arr[i] < 0){
+                    break;
+                }
+                s += arr[i];
+            }
+           
+        maximum = max(s,maximum);
+        }
+
+    cout << "The maximum sum of circular subarray is " << maximum << endl;
+    return 0;
+}
